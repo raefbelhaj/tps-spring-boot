97485c75473d885ca42edab656c367afdcf700ff
raefbelhaj <raefbelhaj54@gmail.com>
2024-01-05 22:39:02 +0100

    tp2spring

---
+++ tp2/demo/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,6 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import net.javaguides.springboot.model.Employee;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+}
